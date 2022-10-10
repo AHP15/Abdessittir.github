25b4a34ad252d4d83af9f59a49c87e45665fa761
abdessittir harkaty <abdessattarharkaty@gmail.com>
2022-10-10 12:39:58 +0100

    Fix stylelint and eslintrc errors

--- script.js
+++ script.js
@@ -1,32 +1,39 @@
-const menuBtn = document.querySelector("#menu-btn");
-const image = document.querySelector(".menu-icon");
-const links = document.querySelector(".header .links");
-const menuLinks = document.querySelectorAll(".menu-link");
-const arrow = document.querySelector(".arrow");
+const menuBtn = document.querySelector('#menu-btn');
 
-console.log(menuBtn.style.display);
-function handleClick(event){
-    menuBtn.classList.toggle("active");
+const image = document.querySelector('.menu-icon');
 
-    if(image.id === 'menu'){
-        image.src = './media/icon.png';
-        image.id = "close";
-        arrow.style.display = "none";
-    }else {
-        image.src = './media/Union.png';
-        image.id = "menu";
-        arrow.style.display = "block";
-    }
-    
-    links.classList.toggle("links_active");
+const links = document.querySelector('.header .links');
+
+const menuLinks = document.querySelectorAll('.menu-link');
+
+const arrow = document.querySelector('.arrow');
+
+function handleClick() {
+  menuBtn.classList.toggle('active');
+
+  if (image.id === 'menu') {
+    image.src = './media/icon.png';
+
+    image.id = 'close';
+
+    arrow.style.display = 'none';
+  } else {
+    image.src = './media/Union.png';
+
+    image.id = 'menu';
+
+    arrow.style.display = 'block';
+  }
+
+  links.classList.toggle('links_active');
 }
 
 menuBtn.addEventListener('click', handleClick);
 
-window.addEventListener("resize", () => {
-    if(window.innerWidth < 768) {
-        menuLinks.forEach((menuLink) => {
-            menuLink.addEventListener('click', handleClick);
-        });
-    }
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    menuLinks.forEach((menuLink) => {
+      menuLink.addEventListener('click', handleClick);
+    });
+  }
 });
