335e87da4c51d6618a0ed82f3496f1f5c5c3694f
Kweeka <mtalbi29@yahoo.com>
2022-10-12 10:50:53 +0300

    Update index.html, Added form error element

--- validate.js
+++ validate.js
@@ -1,5 +1,17 @@
 
 const form = document.querySelector('.contact-form');
+const errorField = document.querySelector('.form-error');
+
+function showErrorMessage(errorMessage) {
+  errorField.style.display = 'flex';
+  const errorEl = `<p>${errorMessage}</p>`
+  errorField.insertAdjacentHTML('beforeend', errorEl);
+}
+
+function resetErrorField() {
+  errorField.style.display = 'none';
+  errorField.textContent = '';
+}
 
 function hasValue(text) {
     return text.trim().length > 0;
@@ -9,44 +21,67 @@ function validateUsername(username) {
   const itHasValue = hasValue(username.value);
 
   if(!itHasValue) {
-
+    const error = 'Username field is empty.'
+    showErrorMessage(error);
+    return false;
   }
 
   const maxLength = username.value.length;
 
   if(maxLength > 30) {
-
+    const error = 'Username must be less than 30 characters.'
+    showErrorMessage(error);
+    return false;
   }
+
+  return true;
 }
 
 function validateEmail(email) {
-  const itHasValue = hasValue(email.value);
+  const emailValue = email.value.trim();
+  const itHasValue = hasValue(emailValue);
 
   if(!itHasValue) {
-
+    const error = 'Email field is empty.'
+    showErrorMessage(error);
+    return false;
   }
 
-  const emailValue = email.value.trim();
   const regexp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   const isLowerCase = /[A-Z]/;
 
-  if (!regexp.test(email) && isLowerCase.test(email)) {
-
+  if (!regexp.test(emailValue)) {
+    const error = 'Email is invalid.'
+    showErrorMessage(error);
+    return false;
   }
 
+  if (isLowerCase.test(emailValue)) {
+    const error = 'Email must be in lowercase letters.'
+    showErrorMessage(error);
+    return false;
+  }
+
+  return true;
 }
 
 function validateMessage(message) {
-    const itHasValue = hasValue(message.value);
-    if(!itHasValue) {
+  const itHasValue = hasValue(message.value);
+  if(!itHasValue) {
+    const error = 'Text message field is empty.'
+    showErrorMessage(error);
+    return false;
+  }
 
-    }
+  const maxLength = message.value.length;
 
-    const maxLength = message.value.length;
+  if(maxLength > 500) {
+    const error = 'Text message must be less than 500 characters.'
+    showErrorMessage(error);
+    return false;
+  }
 
-    if(maxLength > 500) {
-        
-    }
+  return true;
 }
 
 function handleSubmit(event) {
@@ -64,3 +99,4 @@ function handleSubmit(event) {
 }
 console.log(form);
 form.addEventListener('submit', handleSubmit);
+form.addEventListener('input', resetErrorField);
